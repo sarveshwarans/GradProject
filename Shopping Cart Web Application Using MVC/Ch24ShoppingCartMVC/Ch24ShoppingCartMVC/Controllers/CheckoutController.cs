01dabd4d3ef6aa0437c9b145ac02fdece95dcb62
Sarveshwaran <sarveshswaran@gmail.com>
2019-02-06 23:57:33 -0800

    Initial Commit

---
+++ Shopping Cart Web Application Using MVC/Ch24ShoppingCartMVC/Ch24ShoppingCartMVC/Controllers/CheckoutController.cs
@@ -0,0 +1,87 @@
+﻿using Ch24ShoppingCartMVC.Models;
+using Ch24ShoppingCartMVC.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ch24ShoppingCartMVC.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        CartModel cart = new CartModel();
+
+        
+        public RedirectToRouteResult Index()
+        {
+            return RedirectToAction("List/");
+        }
+
+        [HttpGet]
+        public ActionResult List()
+        {
+            CartViewModel model = (CartViewModel)TempData["cart"];
+            if (model == null)
+                model = cart.GetCart();
+            return View(model);
+        }
+        [HttpPost]
+        public RedirectToRouteResult List(OrderViewModel order)
+        {
+            CartViewModel model = cart.GetCart(order.SelectedProduct.ProductID);
+            model.AddedProduct.Quantity = order.SelectedProduct.Quantity;
+            cart.AddToCart(model);
+            TempData["cart"] = model;
+            return RedirectToAction("Index", "Checkout");
+        }
+        [HttpPost]
+        public RedirectToRouteResult SubmitCheckout(CartViewModel model)
+        {
+         
+            try
+            {
+               
+                HalloweenEntities1 halloweenEntities1 = new HalloweenEntities1();
+                halloweenEntities1.Customers.Add(new Customer { Email = model.Email, FirstName=model.FirstName,LastName = model.LastName, Address = model.Address, City = model.City, State = model.State, ZipCode = model.PostalCode, PhoneNumber = model.PhoneNumber });
+                halloweenEntities1.SaveChanges();
+                //return RedirectToAction("PaymentPage", "Checkout");
+
+            }
+            catch(System.Data.Entity.Validation.DbEntityValidationException dbx)
+            {
+
+            }
+            return RedirectToAction("PaymentPage", "Checkout");
+        }
+        [HttpGet]
+        public ActionResult Confirmation()
+        {
+
+            return View();
+        }
+        public ActionResult PaymentPage()
+        {
+            
+            return RedirectToAction("Payment", "Checkout");
+        }
+
+        public ActionResult Payment(CartViewModel model)
+        {
+           
+            return View();
+        }
+
+        public ActionResult PayPal()
+        {
+            return Content("<script>window.location = 'https://www.paypal.com/signin?country.x=US&locale.x=en_US';</script>");
+        }
+
+       public ActionResult ShippingConf(CartViewModel model)
+        {
+            return RedirectToAction("Confirmation", "Checkout");
+        }
+
+    }
+}
---
+++ Shopping Cart Web Application Using MVC/Ch24ShoppingCartMVC/Ch24ShoppingCartMVC/Models/ViewModels/CheckoutViewModel.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ch24ShoppingCartMVC.Models.ViewModels
+{
+    public class CheckoutViewModel
+    {
+        [RegularExpression(@"[0-9]+-", ErrorMessage = "The Credit Card number must be 16 digits")]
+        [Display(Name = "Credit Card Number")]
+        [Required]
+        [StringLength(16, MinimumLength = 16)]
+        public string CreditCardNumber { get; set; }
+
+        [Display(Name = "Cardholder Name")]
+        [Required]
+        [StringLength(60, MinimumLength = 2)]
+        public string CardholderName { get; set; }
+
+
+        [Display(Name = "Card Type")]
+        [Required]
+        [StringLength(6, MinimumLength = 5)]
+        public string CardType { get; set; }
+
+
+        [Display(Name = "Card Provider")]
+        [Required]
+        [StringLength(30, MinimumLength = 4)]
+        public string CardProvider { get; set; }
+
+
+        [Display(Name = "Expiration Date")]
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{MM}")]
+        public DateTime ExpirationDate { get; set; }
+
+
+        [Display(Name = "First Name")]
+        [Required]
+        [StringLength(100, MinimumLength = 4, ErrorMessage = "The Name must be at least 4 - 100 "
+      + "characters long.")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [Required]
+        [StringLength(100, MinimumLength = 4, ErrorMessage = "The Last Name must be at least 4 - 100 "
+        + "characters long.")]
+        public string LastName { get; set; }
+
+
+        [Display(Name = "Address")]
+        [Required]
+        [StringLength(100, MinimumLength = 2)]
+        public string Address { get; set; }
+
+
+        [Display(Name = "City")]
+        [Required]
+        [StringLength(100, MinimumLength = 2)]
+        public string City { get; set; }
+
+
+        [Display(Name = "State")]
+        [Required]
+        [StringLength(5, MinimumLength = 2)]
+        public string State { get; set; }
+
+
+        [Display(Name = "Postal Code")]
+        [Required]
+        [StringLength(11, MinimumLength = 5)]
+        public string PostalCode { get; set; }
+
+
+        [Display(Name = "Country")]
+        [Required]
+        [StringLength(60, MinimumLength = 2)]
+        public string Country { get; set; }
+
+
+        [RegularExpression(@"[0-9]+-", ErrorMessage = "The Phone number must be 10 digits")]
+        [Display(Name = "Phone Number")]
+        [Required]
+        [StringLength(14, MinimumLength = 12)]
+        public string PhoneNumber { get; set; }
+
+
+        [Display(Name = "Email Address")]
+        [Required]
+        [StringLength(100, MinimumLength = 6)]
+        public string Email { get; set; }
+
+    }
+}
